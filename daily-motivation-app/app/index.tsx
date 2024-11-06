82d2bfbde681284ce4fcd572a1e734c3f2c40ced
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-11-06 10:25:09 +0100

    feat: display randomly selected quote from array, add backround image

--- daily-motivation-app/app/index.tsx
+++ daily-motivation-app/app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
       {/* <Text>Motivational quote placeholder</Text> */}
       <MotivationalQuote />
     </View>
